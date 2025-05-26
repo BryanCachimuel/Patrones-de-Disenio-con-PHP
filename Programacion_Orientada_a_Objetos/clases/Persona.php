a12d11e340ca991bfa554f0024890a6312bf6be6
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-25 23:19:24 -0500

    Creando dos clases hijas y se procedio a extender de la clase padre para comprbar el funcionamiento de la Herencia

--- Programacion_Orientada_a_Objetos/clases/Persona.php
+++ Programacion_Orientada_a_Objetos/clases/Persona.php
@@ -2,13 +2,16 @@
 
 class Persona {
     /* propiedades */
-    public $nombre, $apellido, $edad;
+    public $nombre;
+    public $apellido1, $apellido2;
+    public $edad;
 
     /* método constructor */
 
-    public function __construct($nombre, $apellido, $edad){
+    public function __construct($nombre,$apellido1,$apellido2,$edad){
         $this->nombre = $nombre;
-        $this->apellido = $apellido;
+        $this->apellido1 = $apellido1;
+        $this->apellido2 = $apellido2;
         $this->edad = $edad;
     }
 
@@ -19,5 +22,39 @@ class Persona {
 
     public function setNombre($nombre){
         $this->nombre = strtolower($nombre);
+    }
+
+    public function getApellido(){
+        return $this->apellido1 . " " . $this->apellido2;
+    }
+
+    public function setApellido($apellido1, $apellido2){
+        $this->apellido1 = $apellido1;
+        $this->apellido2 = $apellido2;
+    }
+}
+
+/* Herencia */
+class Ecuatoriano extends Persona{
+    public $provincia, $ciudad;
+
+    public function setApellido($apellido1, $apellido2){
+        // forma de extender un métod de la clase padre mediante parent
+        parent::setApellido($apellido1, $apellido2);
+        echo "Los apellidos se asignaron con exito";
+    }
+
+}
+
+class Colombiano extends Persona{
+    public $departamento, $ciudad;
+
+    public function getApellido(){
+        return $this->apellido2 . " " . $this->apellido1;
+    }
+
+     public function setApellido($apellido1, $apellido2){
+        $this->apellido1 = $apellido1;
+        $this->apellido2 = $apellido2;
     }
 }
