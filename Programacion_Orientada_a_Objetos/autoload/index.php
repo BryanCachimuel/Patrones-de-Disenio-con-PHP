9332aa8f3e68322af0e03a9d7660b2f36127a251
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-26 22:58:45 -0500

    Realizando validaciones para ver si el archivo invocado existe y si lo hay lo trae y manda a imprimir las opciones que elija el usuario

--- Programacion_Orientada_a_Objetos/autoload/index.php
+++ Programacion_Orientada_a_Objetos/autoload/index.php
@@ -5,7 +5,16 @@ use Models\Course;
 
 /* función que detecta cada vez que llamamos a una clase */
 spl_autoload_register(function($clase){
-    echo $clase;
+    /*echo str_replace('\\','/', $clase) . ".php";*/
+    /* se reemplaza la barra invertida por la barra normal seguido del nombre de la clase y su extensión de archivo */
+    if (file_exists(str_replace('\\','/', $clase) . ".php")) {
+        require_once str_replace('\\','/', $clase) . ".php";
+    }
 });
 
 $course = new Course;
+$course->mensaje();
+
+echo "<br>";
+$course_controller = new CourseController();
+$course_controller->mensaje();
