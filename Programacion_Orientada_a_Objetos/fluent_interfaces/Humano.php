66ffc78db4f5dbf0f0e3d4f356d49770ae9b5c97
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-27 22:47:18 -0500

    Aprendiendo sobre el proceso de impresión con fluent interfaces

--- Programacion_Orientada_a_Objetos/fluent_interfaces/Humano.php
+++ Programacion_Orientada_a_Objetos/fluent_interfaces/Humano.php
@@ -6,22 +6,38 @@ class Humano {
 
     public function setNombre($nombre){
         $this->nombre = $nombre;
+        return $this;
     }
 
     public function setApellidos($apellido_paterno, $apellido_materno){
         $this->apellido_paterno = $apellido_paterno;
         $this->apellido_materno = $apellido_materno;
+        return $this;
     }
 
     public function setCedula($cedula){
         $this->cedula = $cedula;
+        return $this;
     }
 
     public function imprimirDatos(){
         echo "Nombre: " . $this->nombre . "<br>";
         echo "Apellido paterno: " . $this->apellido_paterno . "<br>";
         echo "Apellido materno: " . $this->apellido_materno . "<br>";
-        echo "Cédula: " . $this->cedula . "<br>";
+        echo "Cédula: " . $this->cedula . "<brfluent>";
     }
 
 }
+
+/* Forma normal de impresión de información */
+$humano = new Humano;
+/*$humano->setNombre("Evelin");
+$humano->setApellidos("Haro","Guaman");
+$humano->setCedula("1008479685");
+$humano->imprimirDatos();*/
+
+/* con fluent interfaces y la aplicación de de los retornos $this en cada método set se hace lo siguiente */
+$humano->setNombre("Evelin")
+       ->setApellidos("Haro","Guaman")
+       ->setCedula("1008479685")
+       ->imprimirDatos();
