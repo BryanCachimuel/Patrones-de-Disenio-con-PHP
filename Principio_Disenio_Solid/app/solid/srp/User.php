84ef360e7d55073e0e2b61263bfff4fd529de48e
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-28 22:44:53 -0500

    Está clase es dividida en dos clases más con el fin de cumplir el principio de responsabilidad única

--- Principio_Disenio_Solid/app/solid/srp/User.php
+++ Principio_Disenio_Solid/app/solid/srp/User.php
@@ -1,7 +1,14 @@
 <?php
 
 /* srp -> principio de responsabilidad unica */
+
 namespace App\solid\srp;
+
+/* 
+    Esta clase tiene 3 responsabilidades y eso incumple con su principio
+    así que estos scripts se dividen en 3 clases para cumplir con el proposito
+    de la responsailidad única
+*/
 
 class User {
     private string $name;
@@ -28,7 +35,19 @@ class User {
         $this->email = $email;
     }  
 
-    
+    /*
+    Este método se traslada a la clase de UserRepository
+
+    public function save(): void {
+        echo"Guardando usuario {$this->name} en la base de datos. </br>";
+    }
+
+    este método se traslada a la clase de EmailService
+
+    public function sendEmail(): void {
+        echo "Enviando correo {$this->email}. </br>";
+    }
+    */
 
 }
 
