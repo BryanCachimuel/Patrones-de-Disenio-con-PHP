e9dafdc8649e100a100b849021a8632345660ffb
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-29 22:39:33 -0500

    Implementando la interfaz MetodoPago y agregando a la clase el método procesar

--- Principio_Disenio_Solid/app/solid/ocp/pagos/Transferencia.php
+++ Principio_Disenio_Solid/app/solid/ocp/pagos/Transferencia.php
@@ -2,6 +2,12 @@
 
 namespace App\solid\ocp\pagos;
 
-class Transferencia {
+use App\solid\ocp\interfaces\MetodoPago;
+
+class Transferencia implements MetodoPago{
     
+    public function procesar(float $monto): void {
+        echo "Procesando pago con transferencia por un monto de $monto </br>";
+    }
+
 }
