75517ed2ec0eb53f1cd3a2b9c6fd7a21beec065b
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-29 22:41:13 -0500

    Convirtiendo a la clase en un modelo no modificable y que se acople al principio abierto cerrado

--- Principio_Disenio_Solid/app/solid/ocp/ProcesadorPagos.php
+++ Principio_Disenio_Solid/app/solid/ocp/ProcesadorPagos.php
@@ -1,27 +1,18 @@
 <?php
 
-/* ocp ->Principio abierto cerrado */
+/* ocp ->Principio abierto cerrado 
+    implica que se tiene libre elección de crear las clases necesarias que esto quiere decir el proceso abierto
+    pero no se puede agregar nada dentro de esta clase eso quiere decir que esta cerrada para su modificación
+*/
 namespace App\solid\ocp;
+
+use App\solid\ocp\interfaces\MetodoPago;
 
 class ProcesadorPagos {
 
-    public function procesarPago(string $metodo, float $monto):void {
-
-        switch ($metodo) {
-            case 'tarjeta':
-                echo "Procesando pago con tarjeta de crédito por un monto de $monto </br>";
-                break;
-
-            case 'transferencia':
-                echo "Procesando pago con transferencia por un monto de $monto </br>";
-                break;
-
-            default:
-                throw new \InvalidArgumentException("Método de pago no soportado: $metodo");
-        }
-
-        echo "Generando orden";
-
+    public function procesarPago(MetodoPago $metodo, float $monto):void {
+        $metodo->procesar($monto);
+        echo "Generando orden <br>";
     }
 
 }
