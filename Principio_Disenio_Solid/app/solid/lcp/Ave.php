e7492c9c1c521dbc3e8d9f4893309b911a4ee461
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-29 22:47:29 -0500

    Aprendiendo a utilizar el principio de sustitución de Liskov

---
+++ Principio_Disenio_Solid/app/solid/lcp/Ave.php
@@ -0,0 +1,13 @@
+<?php
+
+class Ave {
+
+    public function comer(): void {
+        echo "Estoy comiendo <br>";
+    }
+
+    public function volar(): void {
+        echo "Estoy volando <br>";
+    }
+
+}
