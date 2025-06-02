b667f33cfa96122e9133d6c12ba99822011281d5
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-01 22:35:38 -0500

    Creando la interfaz IMesero con sus respectivos métodos y la misma servirá como implementación para la clase Mesero

---
+++ Principio_Disenio_Solid/app/solid/isp/Mesero.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\solid\isp\interfaces\IMesero;
+
+class Mesero implements IMesero {
+
+    public function tomarOrden(): void{
+
+    }
+
+    public function servirComida(): void{
+
+    }
+
+    public function cobrarCuenta(): void{
+
+    }
+
+    public function limpiarMesa(): void {
+
+    }
+
+    public function atenderCliente(): void {
+
+    }
+    
+}
