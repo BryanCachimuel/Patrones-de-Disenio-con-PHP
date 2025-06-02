1873d267662b097c459e3a63d3b1a694636df521
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-02 12:17:38 -0500

    Realizando una prueba de funcionamiento del principio de inversión de depencias y probar el uso práctico del envió de mensajes mediante usuarios

--- Principio_Disenio_Solid/index3.php
+++ Principio_Disenio_Solid/index3.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\solid\dip\EmailSender;
 use App\solid\dip\NotificationService;
+use App\solid\dip\SmsSender;
 use App\solid\dip\User;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $user = new User('Bryan LCL', 'blcl@gmail.com', '0987453689');
 
-$notification = new NotificationService();
+$notification = new NotificationService(new SmsSender());
 $notification->notify($user, "Hola, Bienvenido al mundo de la programación ");
