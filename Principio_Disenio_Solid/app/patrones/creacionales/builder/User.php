816bd4417dae8d660a002373c221b1a92b6f0f87
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-04 11:39:03 -0500

    Aprendiendo sobre como usar el patrón builder mediante el ejemplo de la creación de la clase User

---
+++ Principio_Disenio_Solid/app/patrones/creacionales/builder/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\patrones\creacionales\builder;
+
+class User {
+
+    // se pone el signo de interrogación delante del tipo de dato para que la variable acepte valores nulos
+
+    private ?string $name = null;
+
+    private ?string $address = null;
+    private ?string $phone  = null;
+
+    private ?string $email  = null;
+    private ?string $password  = null;
+
+    public function __construct(?string $name, ?string $address, ?string $phone, ?string $email, ?string $password) {
+        $this->name = $name;
+        $this->address = $address;
+        $this->phone = $phone;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getName(): ?string {
+        return $this->name;
+    }
+
+    public function getAddress(): ?string {
+        return $this->address;
+    }
+
+    public function getPhone(): ?string {
+        return $this->phone;
+    }
+
+    public function getEmail(): ?string {
+        return $this->email;
+    }
+
+    public function getPassword(): ?string {
+        return $this->password;
+    }
+
+}
