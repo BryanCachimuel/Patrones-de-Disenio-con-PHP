f8b0f5f472e177177e43297755b3af81fbba017a
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-05 12:36:00 -0500

    Compobando la efectividad del uso del patrón facade y la disminución de código y la eficiencia del mismo

--- Principio_Disenio_Solid/indexfacade.php
+++ Principio_Disenio_Solid/indexfacade.php
@@ -1,16 +1,20 @@
 <?php
 
+use App\patrones\estructurales\facades\MailerFacade;
 use App\patrones\estructurales\facades\MailSender;
 use App\patrones\estructurales\facades\MessageBuilder;
 use App\patrones\estructurales\facades\SMTPConnection;
 
 require __DIR__ . "/vendor/autoload.php";
 
-$connection = new SMTPConnection();
+/*$connection = new SMTPConnection();
 $connection->authenticate('programadores','123456789');
 
 $message = new MessageBuilder();
 $message->setContent('Esté es un mensaje de prueba');
 
 $mailSender = new MailSender();
-$mailSender->send($connection, $message); 
+$mailSender->send($connection, $message); */
+
+$mailer = new MailerFacade();
+$mailer->send('programadores','123456789','Esté es un mensaje de prueba');
