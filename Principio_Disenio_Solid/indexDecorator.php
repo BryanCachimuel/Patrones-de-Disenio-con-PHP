3a451fd1fc20217c6861f7f65ba280e199339212
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-06 22:37:58 -0500

    Realizando una prueba de funcionamiento de la nueva clase para el patrón decorador

--- Principio_Disenio_Solid/indexDecorator.php
+++ Principio_Disenio_Solid/indexDecorator.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\patrones\estructurales\decorator\CafeSimple;
+use App\patrones\estructurales\decorator\ConCanela;
 use App\patrones\estructurales\decorator\ConLeche;
 
 require __DIR__ . "/vendor/autoload.php";
 
 $bebida = new CafeSimple();
-$bebida = new ConLeche($bebida);
+//$bebida = new ConLeche($bebida);
+$bebida = new ConCanela($bebida);
 
 echo "Descripción: " . $bebida->getDescription() . "<br>";
 echo "Costo: " . $bebida->getCosto() . "<br>";
